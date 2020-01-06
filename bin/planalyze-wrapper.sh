22dbe893f0e1acb81ec8236cedf98666bde8045c
Sam Thursfield <sam@afuera.me.uk>
2020-01-07 00:56:23 +0100

    Planalyzer updates

--- bin/planalyze-wrapper.sh
+++ bin/planalyze-wrapper.sh
@@ -2,7 +2,7 @@
 
 set -e
 
-datadir=$(mktemp -d)
-python3 ~/ESL/tools/planalyzer/src/reader/plan_reader.py  ~/Dropbox/Teaching/2018-08\ -\ Summer\ Intensive/*-{FFCE,FCAE}.docx ~/Dropbox/Teaching/2018-2019/*/*-{BE?,CR?,FPET,FFCE}.docx  --level-map /home/sam/ESL/tools/planalyzer/level_map.json > $datadir/plans.json
-python3 ~/ESL/tools/planalyzer/src/reader/log_reader.py ~/Dropbox/Teaching/2018-2019/Log.docx ~/Dropbox/Teaching/2018-2019/Log\ term1.docx --levels BE2,BE3,CR2,CR3,FPET,FFCE  > $datadir/logs.json
-python3 ~/.local/bin/planalyze-gtk $datadir/plans.json $datadir/logs.json
+exec planalyze-gtk $@ \
+    --plan ~/Dropbox/Teaching/2018-08\ -\ Summer\ Intensive/*-{FFCE,FCAE}.docx ~/Dropbox/Teaching/2018-2019/*/*-{BE?,CR?,FPET,FFCE}.docx /home/sam/Dropbox/Teaching/2019-2020/*/*\ {BE?,CR?,FPET}\ DAY*.docx \
+    --level-map /home/sam/ESL/tools/planalyzer/level_map.json \
+    --log ~/Dropbox/Teaching/2018-2019/Log*.docx ~/Dropbox/Teaching/2019-2020/Log*.docx
