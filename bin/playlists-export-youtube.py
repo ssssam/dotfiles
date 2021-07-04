bb0a46ded83add4051d716c611c951c1ac86fabf
Sam Thursfield <sam@afuera.me.uk>
2021-07-04 23:59:03 +0200

    playlists: Fix youtube playlist export

--- bin/playlists-export-youtube.py
+++ bin/playlists-export-youtube.py
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -p /home/sam/Dropbox/Playlists/Youtube
-cpe spotify export | cpe export-split - /home/sam/Dropbox/Playlists/Youtube
+cpe youtube export ssssam54623 | cpe export-split - /home/sam/Dropbox/Playlists/Youtube
